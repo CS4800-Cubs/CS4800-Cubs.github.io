dfb127c4b56c7d4c2770585fa030eb16a36f43e2
NatalieDinh <NatalieDinh@users.noreply.github.com>
2020-09-16 12:06:06 -0700

    Added personal http controller

---
+++ compucompare/src/main/java/com/compucompare/NatalieHTTPController.java
@@ -0,0 +1,16 @@
+package com.compucompare;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NatalieHTTPController {
+
+    @RequestMapping(value = "/nataliePage", method = RequestMethod.GET)
+    public String getText(){
+        return "<HTML>"
+        +           "<body><h1 style = \"color: lightblue\">Natalie's Page</h1></body>"
+        +       "</HTML>";
+    }
+}
