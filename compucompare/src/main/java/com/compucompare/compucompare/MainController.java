ebc75325e497174a7eee0bd44d94e836faeb23f8
Markus Hernandez <mshernandez505@gmail.com>
2020-09-16 13:15:41 -0700

    Changed MIME type of stringinfo response to application/json.

--- compucompare/src/main/java/com/compucompare/compucompare/MainController.java
+++ compucompare/src/main/java/com/compucompare/compucompare/MainController.java
@@ -2,6 +2,7 @@ package com.compucompare.compucompare;
 
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class MainController
      * @param str A String parameter provided in the request.
      * @return Details about the String in JSON format.
      */
-    @RequestMapping("/stringinfo")
+    @RequestMapping(value = "/stringinfo", produces = MediaType.APPLICATION_JSON_VALUE)
     String stringInfo(@RequestParam(value = "str", defaultValue = "") String str)
     {
         JSONObject stringInfo = new JSONObject();
