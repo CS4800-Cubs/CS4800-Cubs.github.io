91d5561754e0963ebcb711e5c93789d8d23338e0
Markus Hernandez <mshernandez505@gmail.com>
2020-11-18 11:05:49 -0800

    Removed code from previous assignments and other unnecessary code.

--- src/main/java/com/compucompare/compucompare/MainController.java
+++ src/main/java/com/compucompare/compucompare/MainController.java
@@ -21,24 +21,6 @@ public class MainController
     private ComputerRepository laptopRepository;
 
     /**
-     * Gets a laptop by model.
-     *
-     * Only for testing purposes, the client will
-     * give a search query where we will give a list
-     * of laptops rather than a single laptop.
-     * After the client has the list they can
-     * get the laptop by database ID instead.
-     *
-     * @param model The model of the laptop.
-     * @return A laptop object.
-     */
-    @RequestMapping("/getModel")
-    Computer getModel(@RequestParam(value = "model", defaultValue = "") String model)
-    {
-        return laptopRepository.findByModel(model);
-    }
-
-    /**
      * Get Laptop or Computer based on filtered searches and searchbar searches
      *
      * @param query A string that the user has entered into the search bar
@@ -167,37 +149,4 @@ public class MainController
         }
         return comparedComputers;
     }
-
-    /**
-     * This function will act as a temporary request to allow UI team to progress
-     *
-     * @return A JSON object.
-     */
-
-    @RequestMapping("/tempSearch")
-    public Computer returnTempResults() {
-        Set<StorageComponent> drives = new HashSet<>();
-        drives.add(new StorageComponent("Samsung", "EVO", 256, true, true));
-        drives.add(new StorageComponent("WD", "Blue", 1000, false, false));
-        Set<NetworkComponent> interfaces = new HashSet<>();
-        Set<String> supportedWirelessStandards = new HashSet<>();
-        supportedWirelessStandards.add("BT");
-        supportedWirelessStandards.add("802.11a");
-        supportedWirelessStandards.add("802.11b");
-        supportedWirelessStandards.add("802.11g");
-        supportedWirelessStandards.add("802.11n");
-        supportedWirelessStandards.add("802.11ac");
-        supportedWirelessStandards.add("802.11ax");
-        interfaces.add(new NetworkComponent("Intel", "AX200", 2400, true, supportedWirelessStandards));
-        Computer testLaptop = new Computer("HP", "dsuyf7tud", "HP Laptop", "thumbnailUrl", "pageUrl", true,
-                new CPUComponent("Intel", "9750H", 1000, 2000, 4, "x86"),
-                new GPUComponent("Nvidia", "GTX 1660Ti Mobile", 1000),
-                new RAMComponent("Crucial", "Ballistix", 16, 2400, true),
-                drives,
-                interfaces,
-                new DisplayComponent("HP", "8743yevfn", 15, 1920, 1080, 55),
-                new BatteryComponent("Unknown", "Unknown", 5.5, 5000));
-        return testLaptop;
-    }
-
 }
