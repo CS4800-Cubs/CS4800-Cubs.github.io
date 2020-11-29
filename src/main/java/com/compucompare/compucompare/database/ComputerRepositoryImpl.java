997b3a2905f3b46ecf5510ee02e5ba13d922f4aa
Markus Hernandez <mshernandez505@gmail.com>
2020-11-29 13:02:04 -0800

    Inverse query matching for full results.

--- src/main/java/com/compucompare/compucompare/database/ComputerRepositoryImpl.java
+++ src/main/java/com/compucompare/compucompare/database/ComputerRepositoryImpl.java
@@ -11,9 +11,11 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,15 +37,21 @@ public class ComputerRepositoryImpl implements CustomComputerRepository{
 
         List<Predicate> predicates = new ArrayList<>();
 
-        predicates.add(cb.like(cb.lower(computer.get("brand")), "%" + query.toLowerCase() + "%"));
-        predicates.add(cb.like(cb.lower(computer.get("model")), "%" + query.toLowerCase() + "%"));
-        predicates.add(cb.like(cb.lower(computer.get("brand")), "%" + query.toLowerCase() + "%"));
-        predicates.add(cb.like(cb.lower(cpu.get("model")), "%" + query.toLowerCase() + "%"));
-        predicates.add(cb.like(cb.lower(gpu.get("brand")), "%" + query.toLowerCase() + "%"));
-        predicates.add(cb.like(cb.lower(gpu.get("model")), "%" + query.toLowerCase() + "%"));
-        predicates.add(cb.like(cb.lower(display.get("brand")), " %" + query.toLowerCase() + "%"));
+        predicates.add(queryContains(cb, query, computer.get("brand")));
+        predicates.add(queryContains(cb, query, computer.get("model")));
+        predicates.add(queryContains(cb, query, cpu.get("brand")));
+        predicates.add(queryContains(cb, query, cpu.get("model")));
+        predicates.add(queryContains(cb, query, gpu.get("brand")));
+        predicates.add(queryContains(cb, query, gpu.get("model")));
+        predicates.add(queryContains(cb, query, display.get("brand")));
 
         cq.where(cb.or(predicates.toArray(new Predicate[predicates.size()])));
         return em.createQuery(cq).getResultList();
+    }
+
+    private static Predicate queryContains(CriteriaBuilder cb, String query, Expression<String> expression)
+    {
+        return cb.like(cb.literal(query.toLowerCase()),
+                       cb.concat(cb.concat("%", cb.lower(expression)), "%"));
     }
 }
